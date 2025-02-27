e89eb2fa6235e4fdb574bf264e464b09f6ac5fdf
Reynaldo Allison <Reynaldo341@outlook.com>
2025-02-26 23:55:36 -0500

    This is the final push and my completed assignment

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from datetime import datetime
 
 
 ###
 # Routing for your application.
 ###
-
 @app.route('/')
 def home():
     """Render website's home page."""
@@ -15,12 +15,36 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Reynaldo M. Allison (620146955)")
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+
+def format_date_joined(date):
+    """Formats the date as Month, Year (e.g., Feb, 2021)"""
+    formatted_date = date.strftime("%B, %Y")  
+    print("Joined " + formatted_date)  
+    return date.strftime("%b, %Y")  
+
+
+@app.route('/profile')
+def profile():
+    user_info = {
+        "full_name": "Reynaldo Allison",
+        "username": "Reynaldostudent1",
+        "location": "Kingston, Jamaica",
+        "date_joined": format_date_joined(datetime(2025, 2, 26)),
+        "bio": "Data Scientist | Tech Enthusiast | Anime Nerd",
+        "posts": 0,
+        "followers": -4,
+        "following": 2,
+        "profile_picture": url_for('static', filename='PXL_20240227_182707417.MP.jpg')
+    }
+    return render_template('profile.html', user=user_info)
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
